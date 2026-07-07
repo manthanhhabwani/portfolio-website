6101682ff4705c70a4442def5f4e992b90e2b82e
manthanhhabwani <manthanhabwani17@gmail.com>
2026-07-07 21:19:47 +0530

    Started with the JavaScript

---
+++ script.js
@@ -0,0 +1,5 @@
+const menuToggle = document.querySelector(".menu-toggle");
+const mobileMenu = document.querySelector(".mobile-menu");
+menuToggle.addEventListener("click",function () {
+    console.log("Button Clicked!");
+});
