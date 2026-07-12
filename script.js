e23ce8ac8eb381ca5fe6549713c4276a94561d5e
manthanhhabwani <manthanhabwani17@gmail.com>
2026-07-12 23:26:19 +0530

    add animation to menu toggle for mobile screen

--- script.js
+++ script.js
@@ -2,10 +2,11 @@ const menuToggle = document.querySelector(".menu-toggle");
 const mobileMenu = document.querySelector(".mobile-menu");
 menuToggle.addEventListener("click", function () {
   mobileMenu.classList.toggle("active");
+  menuToggle.classList.toggle("active");
 });
 const mobileLinks = document.querySelectorAll(".mobile-menu a");
-mobileLinks.forEach(function(link) {
-    link.addEventListener("click", function(){
-        mobileMenu.classList.remove("active");
-    });
+mobileLinks.forEach(function (link) {
+  link.addEventListener("click", function () {
+    mobileMenu.classList.remove("active");
+  });
 });
