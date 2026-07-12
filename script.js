6193ee04b1d9ee1dd8ce76d9def99cbd29e23f01
manthanhhabwani <manthanhabwani17@gmail.com>
2026-07-12 20:58:25 +0530

    make the hamburger toggle working

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 const menuToggle = document.querySelector(".menu-toggle");
 const mobileMenu = document.querySelector(".mobile-menu");
-menuToggle.addEventListener("click",function () {
-    console.log("Button Clicked!");
+menuToggle.addEventListener("click", function () {
+  mobileMenu.classList.toggle("active");
+});
+const mobileLinks = document.querySelectorAll(".mobile-menu a");
+mobileLinks.forEach(function(link) {
+    link.addEventListener("click", function(){
+        mobileMenu.classList.remove("active");
+    });
 });
