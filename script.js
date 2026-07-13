d12ad312e4565b6b09a5996e2be11b1f77bdb630
manthanhhabwani <manthanhabwani17@gmail.com>
2026-07-13 22:48:53 +0530

    added outside click exit

--- script.js
+++ script.js
@@ -8,5 +8,15 @@ const mobileLinks = document.querySelectorAll(".mobile-menu a");
 mobileLinks.forEach(function (link) {
   link.addEventListener("click", function () {
     mobileMenu.classList.remove("active");
+    menuToggle.classList.toggle("active");
   });
+});
+document.addEventListener("click", function (event) {
+  if (
+    !mobileMenu.contains(event.target) &&
+    !menuToggle.contains(event.target)
+  ) {
+    mobileMenu.classList.remove("active");
+    menuToggle.classList.remove("active");
+  }
 });
